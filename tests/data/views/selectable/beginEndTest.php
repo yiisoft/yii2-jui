9e972ecb87917213be972a963b8894c1fe8a6a34
Miguel fernandez <mfernandez@qashops.com>
2016-10-05 18:31:28 +0200

    Added test and CHANGELOG line

---
+++ tests/data/views/selectable/beginEndTest.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this \yii\web\View */
+?>
+<?php \yii\jui\Selectable::begin([
+    'clientOptions' => [
+        'filter' => 'my-selectable-item',
+         'tolerance' => 'fit',
+     ],
+ ]); ?>
+<ul>
+      <li class="my-selectable-item">Item 1</li>
+      <li class="my-selectable-item">Item 2</li>
+      <li class="no-selectable-item">Item 3</li>
+      <li class="my-selectable-item">Item 4</li>
+ </ul>
+ <div>
+      <div>
+          <div class="my-selectable-item">Another item </div>
+      </div>
+ </div>
+<?php \yii\jui\Selectable::end(); ?>
