6b8b8e3549ce3ebcb35b9b6ba87f0f256c4d5f66
Alexander Makarov <sam@rmcreative.ru>
2017-04-19 17:25:26 +0300

    Fix phpunit 6 compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/jui', __DIR__);
 Yii::setAlias('@yii/jui', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
